3d10a75e145e8be61ce150927fc8f177513e39f4
eusouobn <86791806+eusouobn@users.noreply.github.com>
2022-05-02 13:28:14 -0300

    Update mount-steam.sh

--- mount-steam.sh
+++ mount-steam.sh
@@ -3,5 +3,5 @@
 
 sudo blkid -s UUID -o value /dev/sdb1 | while read line
 do
-echo '\nUUID='$line /steam  btrfs  defaults  0 0 | sudo tee -a /etc/fstab
+echo '\nUUID='$line /steam  btrfs  defaults  0 0' | sudo tee -a /etc/fstab
 done
